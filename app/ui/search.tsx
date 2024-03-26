3b56247d32fd03818ef3bec5bc6f4ded2deade14
Bách Nguyễn <105241006+bachgpa@users.noreply.github.com>
2024-03-26 07:04:41 +0700

    create Invoices

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -7,15 +7,25 @@ import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
+  // hàm replace được cung cấp bởi useRouter,
+  //giúp đổi url mà không tải lại trang
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string) => {
+    //xác nhận lấy query từ input
+    console.log(`seaching ... ${term}`);
+
+    //lấy chuyển searchParams thành url ojb có thể tương tác
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    //append query nếu có, xóa nếu không có query
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    //gắn pathname + ? + query để tìm kiếm bằng url
     replace(`${pathname}?${params.toString()}`);
   }, 300);
   return (
@@ -29,6 +39,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        //đồng bộ từ url tới search input
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
